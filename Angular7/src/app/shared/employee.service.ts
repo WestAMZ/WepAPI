c5509e71ce04e141b308586502a64049cffca709
MSCorpProgramador02 <programador02@mscorp.group>
2019-05-20 13:24:19 -0600

    Html components employee

--- Angular7/src/app/shared/employee.service.ts
+++ Angular7/src/app/shared/employee.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  //Declaración de propieada formData de tipo Employee
+  formData : Employee;
 
   constructor() { }
 }
