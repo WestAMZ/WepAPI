db3a0643718f605740ce59f633a1a915aed1153d
MSCorpProgramador02 <programador02@mscorp.group>
2019-05-24 12:58:47 -0600

    Form reset working

--- Angular7/src/app/employees/employee/employee.component.ts
+++ Angular7/src/app/employees/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
@@ -11,6 +12,23 @@ export class EmployeeComponent implements OnInit {
   constructor(private service :EmployeeService) { }
 
   ngOnInit() {
+    //Calling the reset form method that is below
+    this.resetForm();
+  }
+  //Initialize  fomrData 
+  resetForm(form?: NgForm)
+  {
+    if(form != null)
+    {
+      form.resetForm();
+    }
+    this.service.formData = {
+        EmployeeID = null,
+        FullName = '',
+        Position = '',
+        EMPCode = '',
+        Mobile = ''
+    }
   }
 
 }
